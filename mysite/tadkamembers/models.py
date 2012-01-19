21a83ef097b9938640dca3a2e390a20760160e89
Anjan Karanam <anjanbk@gmail.com>
2012-01-19 16:35:55 -0500

    Member profiles have been enhanced

--- mysite/tadkamembers/models.py
+++ mysite/tadkamembers/models.py
@@ -6,8 +6,10 @@ from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(User, related_name='tadka_user')
-
-	bio = models.CharField(max_length=2000, default="Foobar")
+	bio = models.CharField(max_length=2000, default=" ")
+	year = models.CharField(max_length=20, default=" ")
+	major = models.CharField(max_length=50, default=" ")
+	vocalparts = models.CharField(max_length=100, default="")
 
 def create_user_profile(sender, instance, created, **kwargs):
 	if created:
